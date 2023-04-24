77e25810817e9fbbf089aa1b6f984193f7ea3255
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2023-04-24 16:53:50 +0200

    Add some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className=" bg-white  border-t w-full">
-      <div className="flex sm:flex-col sm:text-center justify-between max-w-6xl px-4 py-2 m-auto">
+      <div className="flex sm:flex-col sm:text-center justify-between max-w-6xl px-3 py-2 m-auto">
         <h4 className="text-[#1e1d28] text-[12.25px] leading-[19.9675px]">
           © 2023 NEWS FLOW, All rights reserved
         </h4>
